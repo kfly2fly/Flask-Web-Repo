523ab0972fc9281278697d09200dc5cc393a08d4
Keenan <k.p.flynn70@gmail.com>
2021-10-16 19:17:52 -0500

    Created database using sqlalchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,42 @@
 from flask import Flask, redirect, url_for, render_template, request, flash, session
 from forms import RegistrationForm, LoginForm
-from datetime import timedelta
+from datetime import timedelta, datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '25b720ef729a53baf9030f28fd179129'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.permanent_session_lifetime = timedelta(days=5)
+
+# Database stuff
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+
+
 
 posts = [
     {
