cf16e46984a2b707690022212b97d48ee441ad04
Keenan Flynn <k.p.flynn70@gmail.com>
2024-02-12 18:43:52 -0600

    refactor(gitignore): remove cached files

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -18,5 +18,13 @@ def about():
 @main.route("/blog")
 def blog():
     page = request.args.get('page', 1, type=int)
-    posts=Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    posts = Post.query.order_by(
+        Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('blog.html', posts=posts)
+
+
+
+
+@main.route("/home2")
+def home2():
+    return render_template('home2.html')
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flaskblog import create_app
 
+# Function defined in __init__.py
 app = create_app()
 
 if __name__ == '__main__':
